1347b7911b58dd5592e2b7ff1f1f7fe01a48b732
projetoscti28 <suporteatmovision.cti@gmail.com>
2024-10-25 10:38:21 -0300

    Add files via upload

--- telas-recuperar-senha/tela-insira-email/script.js
+++ telas-recuperar-senha/tela-insira-email/script.js
@@ -1,13 +1,17 @@
+// Adiciona um ouvinte de eventos para detectar o movimento do mouse
 document.addEventListener('mousemove', function(e) {
-    if (e.buttons !== 1) return; //Só cria rastros quando o botão do mouse estiver pressionado (talvez removemos isso, depende do Bikas)
+    // Só cria rastros quando o botão do mouse estiver pressionado
+    if (e.buttons !== 1) return;
 
+    // Cria um novo elemento div para o rastro visual
     const trail = document.createElement('div');
-    trail.className = 'trail'; //traços
-    trail.style.left = `${e.pageX}px`;
-    trail.style.top = `${e.pageY}px`;
-    document.body.appendChild(trail);
+    trail.className = 'trail'; // Aplica a classe de estilo 'trail'
+    trail.style.left = `${e.pageX}px`; // Define a posição horizontal do rastro
+    trail.style.top = `${e.pageY}px`;  // Define a posição vertical do rastro
+    document.body.appendChild(trail); // Adiciona o rastro ao corpo da página
 
+    // Remove o rastro após 2.5 segundos para evitar acúmulo de elementos
     setTimeout(() => {
         trail.remove();
-    }, 2500); //Remove o rastro após 2.5 segundos
+    }, 2500);
 });
